221a4a6932874c096adbf9c888ff4215a62d319d
RaulDuVrau <raulvilefran@gmail.com>
2019-05-10 21:25:33 -0300

    raul e nathan

---
+++ RLNTbanco.sql
@@ -0,0 +1,14 @@
+go
+create database raulnathan
+go
+go
+use raulnathan
+go
+
+create table cadastro
+(nome varchar(50),
+ codigo varchar(50),
+ cep varchar(50),
+ endereco varchar(70),
+ bairro varchar(70),
+ cidade varchar(20));
